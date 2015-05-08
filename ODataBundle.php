bfa78d9593d896d2db01e76be300db7a56170a39
Mark Schmale <masch@masch.it>
2015-05-08 21:01:07 +0200

    added TestApp

---
+++ ODataBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace masch\Bundle\ODataBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle
+ *
+ * @author Mark Schmale <masch@masch.it>
+ */
+class ODataBundle extends Bundle
+{
+
+}
---
+++ Tests/Functional/app/README.md
@@ -0,0 +1,4 @@
+odata-test-app
+==============
+
+A Symfony project created on May 8, 2015, 8:28 pm.
